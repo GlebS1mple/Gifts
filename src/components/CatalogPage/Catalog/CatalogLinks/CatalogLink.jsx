ed7424fcb913ab8344ff49f31638303eb577150e
GlebS1mple <gleb.gonchar.77@mail.ru>
2022-06-09 18:02:57 +0300

    Updates

--- src/components/CatalogPage/Catalog/CatalogLinks/CatalogLink.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CatalogLink = ({ text, onClick, name }) => {
-    return (
-        <li onClick={onClick} className={name}>{text}</li>
-    );
-};
-
-export default React.memo(CatalogLink);
