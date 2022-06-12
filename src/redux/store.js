740007061bb17ce7d8f043616ccec4a6949c2b54
GlebS1mple <gleb.gonchar.77@mail.ru>
2022-06-12 17:13:22 +0300

    Handling errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import thunkMiddleware from "redux-thunk";
 import catalogReducer from "./catalogReducer";
 import aboutReducer from "./aboutReducer";
 import cartReducer from "./cartReducer";
-import { loadState, saveState } from './localStorage';
+//import { loadState, saveState } from './localStorage';
 
 
 
@@ -14,18 +14,18 @@ let reducers = combineReducers({
     about: aboutReducer,
     cart: cartReducer,
 });
-const persistedState = loadState();
+//const persistedState = loadState();
 //const persistedState = localStorage.getItem("cartState") ? JSON.parse(localStorage.getItem("cartState")) : {};
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, persistedState, /* preloadedState, */ composeEnhancers(
+const store = createStore(reducers, /*persistedState,  preloadedState, */ composeEnhancers(
     applyMiddleware(thunkMiddleware)
 ));
-store.subscribe(() => {
+/*store.subscribe(() => {
     saveState({
         cartProducts: store.getState().cart.cartProducts
     })
     //localStorage.setItem("cartState", JSON.stringify(store.getState().cart));
-});
+});*/
 //console.log(persistedState)
 
 //let store = createStore(reducers, applyMiddleware(thunkMiddleware));
